f3d48817e99be0e0dab660713d4b5b1868ef90f3
Aayush Trivedi <aayush.t200@gmail.com>
2026-04-23 13:40:43 +0530

    linted code

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,43 +1,44 @@
 // webpack.prod.js
-import path from "path";
-import { fileURLToPath } from "url";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import path from "path"
+import { fileURLToPath } from "url"
 
 // __dirname replacement for ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+	mode: "production",
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	devtool: "eval-source-map",
+	devServer: {
+		watchFiles: ["./src/template.html"],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+}
